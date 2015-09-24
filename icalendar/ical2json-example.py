4a2cde5fff909c1da4fe6215435a97ad6f7d4fc4
Kevin Baker <shagol@gmail.com>
2015-09-23 17:51:12 -0700

    new version of ical2json. now using "default" parameter to pass a subroutine into simplejson.dumps to catch and convert the date-time objects. much cleaner

--- icalendar/ical2json-example.py
+++ icalendar/ical2json-example.py
@@ -10,17 +10,16 @@ import xmaintnote # XXX kbaker's hacks for BCOP
 from datetime import datetime
 import pytz
 
+def encode_vDDDTypes(obj):
+    if isinstance(obj, icalendar.prop.vDDDTypes):
+        # convert vDDDTypes - date/time types to strings
+        return unicode(obj.to_ical())
+    raise TypeError(repr(o) + " is not JSON serializable")
+
 
 def ical2json(cal):
     data = {}
-    data[cal.name] = {}
-
-    for item in cal.items():
-        if isinstance(item[1], icalendar.prop.vDDDTypes):
-            # convert vDDDTypes - date/time types to strings
-            data[cal.name][item[0]] = unicode(item[1].to_ical())
-        else:
-            data[cal.name][item[0]] = unicode(item[1])
+    data[cal.name] = dict(cal.items()); 
 
     for component in cal.subcomponents:
         if not data[cal.name].has_key(component.name):
@@ -28,18 +27,13 @@ def ical2json(cal):
 
         comp_obj = {}
         for item in component.items():
-            if isinstance(item[1], icalendar.prop.vDDDTypes):
-                # convert vDDDTypes - date/time types to strings
-                comp_obj[item[0]] = unicode(item[1].to_ical())
-            else:
-                comp_obj[item[0]] = unicode(item[1])
+            comp_obj[item[0]] = unicode(item[1])
 
         data[cal.name][component.name].append(comp_obj)
 
-    json = _json.dumps(data, sort_keys=True, indent='    ')
+    json = _json.dumps(data, default=encode_vDDDTypes, sort_keys=True, indent='    ')
 
     return json
-
 
 def display(cal):
     return cal.to_ical().replace('\r\n', '\n').strip()
